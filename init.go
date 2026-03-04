fdcc5fd6ab752b3cafbeee6fe4c231df0a11b825
quanchao <quanchao@jwzg.com>
2026-03-04 10:06:54 +0800

    更新

--- init.go
+++ init.go
@@ -2,24 +2,25 @@ package cachex
 
 import (
 	"github.com/go-xuan/configx"
+	"github.com/go-xuan/utilx/errorx"
 	log "github.com/sirupsen/logrus"
 )
 
 func init() {
 	RegisterClientBuilder("local", LocalClientBuilder) // 注册本地缓存客户端构建器
 	RegisterClientBuilder("redis", RedisClientBuilder) // 注册redis缓存客户端构建器
-	Init()                                             // 初始化缓存
 }
 
-func Init() {
+func Initialize() error {
 	logger := log.WithField("package", "cachex")
 	if err := configx.LoadConfigurator(&Configs{}); err == nil && Initialized() {
-		logger.Info("initialized success")
-		return
+		logger.Info("initialize success")
+		return nil
 	}
 	if err := configx.LoadConfigurator(&Config{}); err == nil && Initialized() {
-		logger.Info("initialized success")
-		return
+		logger.Info("initialize success")
+		return nil
 	}
-	logger.Warn("initialized failed")
+	logger.Warn("initialize failed")
+	return errorx.New("initialize cachex failed")
 }
